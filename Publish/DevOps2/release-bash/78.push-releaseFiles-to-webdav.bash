8ef273570ed22790a95d81701b34d40670683f51
lith <LithWang@yeah.net>
2024-01-14 18:26:41 +0800

    sava releaseFiles to WebDav

---
+++ Publish/DevOps2/release-bash/78.push-releaseFiles-to-webdav.bash
@@ -0,0 +1,46 @@
+set -e
+
+
+#---------------------------------------------------------------------
+# args
+args_="
+
+export basePath=/root/temp/svn
+
+export APPNAME=Vit.Library
+export appVersion=2.2.14-ki7
+
+export WebDav_BaseUrl="https://pan.lith.cloud:4/remote.php/dav/files/release/releaseFile/ki_jenkins"
+export WebDav_User="username:pwd"
+
+# "
+
+
+
+
+#---------------------------------------------------------------------
+echo "78.push-releaseFiles-to-webdav.bash -> #1 create dir"
+
+docker run -i --rm curlimages/curl sh -c "curl -X MKCOL -u \"$WebDav_User\" \"$WebDav_BaseUrl/$APPNAME\" " || true
+docker run -i --rm curlimages/curl sh -c "curl -X MKCOL -u \"$WebDav_User\" \"$WebDav_BaseUrl/$APPNAME/$appVersion\" " || true
+
+#---------------------------------------------------------------------
+echo "78.push-releaseFiles-to-webdav.bash -> #2 push release files"
+
+docker run -i --rm \
+-v "$basePath/Publish/release/release-zip":/releaseFiles \
+curlimages/curl \
+sh -c "
+cd /releaseFiles
+for file in /releaseFiles/*
+do
+    echo ''
+    echo '----------------------------'
+    fileName=\"\${file##*/}\"
+    echo push file: \$fileName
+    curl -X PUT -u \"$WebDav_User\" \"$WebDav_BaseUrl/$APPNAME/$appVersion/\$fileName\" --data-binary "/releaseFiles/\$fileName"
+done
+"
+
+#---------------------------------------------------------------------
+echo "78.push-releaseFiles-to-webdav.bash -> #3 success"
