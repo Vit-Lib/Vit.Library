c336c67df2a53abe60a36a45f89d2aa1804446ab
Lith <LithWang@outlook.com>
2024-06-30 21:57:56 +0800

    upgrade library

--- Vit.OnlineUpgrade/Test/Vit.OnlineUpgrade.NetCoreApp21.Demo/Program.cs
+++ Vit.OnlineUpgrade/Test/Vit.OnlineUpgrade.NetCoreApp21.Demo/Program.cs
@@ -1,17 +1,30 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace Vit.OnlineUpgrade.Netcore.Demo
+using Vit.Extensions;
+
+namespace App
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // 加载在线升级api（从appsettings.json中读取配置。如不需要在线升级，可以不加载，在线升级地址为 /api/onlineupgrade/index.html ）
+            app.Use_OnlineUpgrade();
+
+
+            //app.UseAuthorization();
+            app.MapControllers();
+
+            app.Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
     }
 }
